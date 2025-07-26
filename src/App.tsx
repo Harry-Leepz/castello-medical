bd376d8eaf16dbb5ba73ea07507edbbb2da64f05
Harry Dhillon <hdhillon478@gmail.com>
2025-07-26 12:50:09 +0100

    feat: replace placeholder content with Home component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
 
+import Home from "./pages";
+
 function App() {
-  return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
