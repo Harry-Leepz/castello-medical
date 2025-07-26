006b3a3cf8ad381bfc13c67ef8e2f29ef3fcf530
Harry Dhillon <hdhillon478@gmail.com>
2025-07-26 13:42:01 +0100

    feat: integrate FilterForm into Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,32 @@
+import FilterForm from "../components/filter-form";
 import Header from "../components/header";
+import MainLayout from "../components/layout";
+
+import { usePubMedExplorer } from "../lib/context/usePubMedExplorer";
 
 export default function Home() {
+  const {
+    filters,
+    setFilters,
+
+    isLoading,
+    error,
+  } = usePubMedExplorer();
   return (
     <div className='h-screen w-full flex flex-col'>
       <Header />
+
+      <MainLayout
+        sidebar={<FilterForm initialFilters={filters} onSubmit={setFilters} />}
+      >
+        {isLoading ? (
+          <p className='p-4 text-gray-600'>Loading articles...</p>
+        ) : error ? (
+          <p className='p-4 text-red-600'>Error loading articles.</p>
+        ) : (
+          <></>
+        )}
+      </MainLayout>
     </div>
   );
 }
