81a5afabd4eac2dd6b7c89e79f5997d3bbf360da
Harry Dhillon <hdhillon478@gmail.com>
2025-07-26 12:49:54 +0100

    feat: create Home page component with Header integration

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../components/header";
+
+export default function Home() {
+  return (
+    <div className='h-screen w-full flex flex-col'>
+      <Header />
+    </div>
+  );
+}
