71114e293cee845b8296d6090be6bebf0d6e5826
Harry Dhillon <hdhillon478@gmail.com>
2025-07-26 13:41:18 +0100

    feat: wrap App component with PubMedExplorerProvider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PubMedExplorerProvider from "./lib/context/PubMedExplorerProvider.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <PubMedExplorerProvider>
+        <App />
+      </PubMedExplorerProvider>
     </QueryClientProvider>
   </StrictMode>
 );
