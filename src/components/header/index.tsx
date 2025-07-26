52b7b917162ed9af3689d45794b7695a7799b13a
Harry Dhillon <hdhillon478@gmail.com>
2025-07-26 12:49:06 +0100

    feat: add Header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className='flex items-center castello-light p-10 shadow-md h-25'>
+      <h1 className='castello-dark text-3xl font-bold'>
+        PubMed Article Explorer
+      </h1>
+    </header>
+  );
+}
